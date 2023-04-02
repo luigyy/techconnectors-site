f8e5254417965180445609adeb8405b718af958c
luigyy <luigy.valverde@ucr.ac.cr>
2023-04-01 18:26:42 -0600

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  return (
+    <div className=" flex justify-between border-b border-b-zinc-400/40 bg-white px-5 py-2 font-semibold shadow-sm">
+      {/* left */}
+      <div className="flex items-center tracking-tight">
+        <span>
+          <img src="techconnectors-logo.png" className="w-8" alt="" />
+        </span>
+        <p className=" text-lg">TechConnectors</p>
+      </div>
+      {/* left */}
+
+      {/* right  */}
+      <div className="flex items-center gap-4 tracking-tight">
+        <p className="text-lg">Servicios</p>
+        <p className="text-lg">Casos de uso</p>
+        <p className="text-lg">Contáctanos</p>
+      </div>
+      {/* right  */}
+    </div>
+  );
+};
+
+export default Navbar;
