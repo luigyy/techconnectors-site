d13caafe8cb1e192049b1d17e2c4b3d4886bcec2
luigyy <luigy.valverde@ucr.ac.cr>
2023-05-11 00:23:17 -0600

    added some descriptions for asistentes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,19 +17,19 @@ const NavbarModal = ({
     <div>
       {title ? (
         <div className="fixed inset-0 z-50  bg-zinc-800/40 opacity-100 backdrop-blur-sm">
-          <div className="scale-10 fixed inset-x-4 top-8 z-50 mx-auto w-[90%] max-w-[800px] origin-top rounded-3xl bg-white p-8 opacity-100 ring-1 ring-zinc-900/5">
+          <div className="scale-10 fixed inset-x-4 top-8 z-50 mx-auto max-h-[80%] w-[90%] max-w-[800px]  origin-top overflow-y-scroll rounded-3xl bg-white p-8 opacity-100 ring-1 ring-zinc-900/5">
             <div className="flex items-center justify-center">
-              <h1 className=" mx-auto mb-10 text-center font-fjalla text-4xl tracking-wider text-zinc-700">
-                Navigation
+              <h1 className=" mx-auto mb-10 text-center font-fjalla text-lg tracking-wider text-zinc-700 md:text-4xl">
+                {title}
               </h1>
               <button
-                className="absolute right-4 top-4 rounded-full  p-5"
+                className="absolute right-0 top-4 rounded-full p-5  md:right-4"
                 onClick={() => setModal({ title: "", description: "" })}
               >
                 <h1 className=" text-5xl font-semibold">X</h1>
               </button>
             </div>
-            <p className="px-2 text-xl tracking-wide">{description}</p>
+            <p className="px-2 tracking-wide md:text-xl">{description}</p>
           </div>
         </div>
       ) : null}
--- src/pages/servicios/rpa.tsx
+++ src/pages/servicios/rpa.tsx
@@ -3,38 +3,37 @@ import { useState } from "react";
 import Modal from "../../components/Modal";
 
 //
-const asistentesDigitales = () => [
-  {
-    title: "Financieros",
-    description: "",
-  },
+const asistentesDigitales = [
   {
     title: "Cotización a cobro (QTC)",
-    description: "",
+    description:
+      "Cada negocio necesita vender para sobrevivir. Los problemas en el lado de las operaciones de venta pueden resultar en quejas de los clientes o ventas a precios reducidos debido a errores administrativos. La automatización del proceso completo de operaciones de venta elimina estos errores y brinda un servicio rápido a sus clientes. Al automatizar el proceso, las ventas se procesan más rápido que mediante procesos manuales, lo que permite que los clientes reciban las facturas de manera más oportuna y mejore el flujo de efectivo. Además, este proceso puede ser más fácil de automatizar, ya que se basa principalmente en datos estructurados de la empresa para generar facturas que se enviarán a los clientes",
   },
   {
     title: "Órdenes de compra  (Procure to pay)",
-    description: "",
+    description:
+      "Dado que el proceso de compra a pago implica extraer datos de facturas y pagos de múltiples sistemas como el planificador de recursos empresariales (ERP), la gestión de relaciones con clientes (CRM), bancos, proveedores y empresas de logística, y dado que no todos estos sistemas proporcionan métodos de integración fáciles, a menudo involucra cierto grado de trabajo manual. Los bots RPA pueden llenar las brechas de integración. Al trabajar en el front-end, pueden ofrecer una forma sencilla de automatizar las integraciones.Con un proceso de compra a pago totalmente automatizado, puede garantizar el cumplimiento de las mejores prácticas de adquisición y tener una única fuente de verdad para todas las transacciones",
   },
   {
     title: "Integración de clientes",
-    description: "",
+    description:
+      "El proceso de integración de clientes es crucial para las empresas B2C, ya que permite reducir la deserción y facilitar que los clientes comiencen a utilizar los productos. Mediante el uso de tecnologías como OCR y automatización cognitiva, es posible realizar de manera instantánea muchas de las tareas asociadas a la integración de clientes, incluso en empresas que dependen de sistemas heredados. Esto tiene un impacto significativo en la experiencia del cliente, mejorándola notablemente. Hemos logrado implementar con éxito el Procesamiento Directo (STP) en el proceso de integración de clientes de un importante banco, lo que ha permitido reducir el tiempo de incorporación de 20 días a tan solo  minutos. Esta optimización ha generado un aumento significativo en la eficiencia y satisfacción de los clientes",
   },
   {
     title: "Migración de datos e ingreso",
-    description: "",
+    description:
+      "Los sistemas heredados continúan desempeñando un papel crítico en las empresas. Por ejemplo, los sistemas de facturación heredados deben interactuar con otros sistemas y es posible que estos no tengan la capacidad de extraer datos relevantes a través de API. En tales casos, los empleados realizan la migración de datos manualmente utilizando formatos como CSV. Sin embargo, la RPA puede evitar el trabajo manual y los posibles errores administrativos asociados.Además, contar con sistemas que mantengan los datos actualizados permite realizar un análisis más preciso y tomar decisiones más informadas. En un mundo en el que incluso el área de marketing cuenta con 5000 aplicaciones para elegir, la RPA puede ayudar a integrar estas aplicaciones y permitir un análisis más holístico.",
   },
-  {
-    title: "Seguros",
-    description: "",
-  },
+
   {
     title: "Procesamiento de Préstamos",
-    description: "",
+    description:
+      "Al igual que con la mayoría de las tareas de procesamiento de documentos, este proceso es adecuado para la automatización mediante RPA, ya que la lógica empresarial compleja se puede integrar en los robots que automatizan parcialmente las decisiones de préstamos y los procesos manuales que siguen a dicha decisión.",
   },
   {
     title: "Ejecución Comercial",
-    description: "",
+    description:
+      "En los casos en los que los sistemas heredados no pueden manejar órdenes de compra complejas, los bots RPA pueden ser de gran ayuda. Sin embargo, esta solución es más una medida temporal, ya que a largo plazo, migrar hacia un sistema de comercio sofisticado y capaz sería una inversión acertada, ya que podría mejorar el comercio y reducir la carga de los operadores.",
   },
   {
     title: "Conciliación de cuentas bancarias",
@@ -42,32 +41,30 @@ const asistentesDigitales = () => [
   },
   {
     title: "Procesamiento de facturas",
-    description: "",
+    description:
+      "Los sistemas heredados continúan desempeñando un papel crítico en las empresas. Por ejemplo, los sistemas de facturación heredados deben interactuar con otros sistemas y es posible que estos no tengan la capacidad de extraer datos relevantes a través de API. En tales casos, los empleados realizan la migración de datos manualmente utilizando formatos como CSV. Sin embargo, la RPA puede evitar el trabajo manual y los posibles errores administrativos asociados. Además, contar con sistemas que mantengan los datos actualizados permite realizar un análisis más preciso y tomar decisiones más informadas. En un mundo en el que incluso el área de marketing cuenta con 5000 aplicaciones para elegir, la RPA puede ayudar a integrar estas aplicaciones y permitir un análisis más holístico",
   },
-  {
-    title: "Operación",
-    description: "",
-  },
+
   {
     title: "Gestión de gastos",
-    description: "",
+    description:
+      "La gestión de gastos implica el seguimiento y control de los gastos incurridos por la empresa, incluidos los reembolsos de empleados y los gastos relacionados con viajes y entretenimiento. La automatización de este proceso permite capturar y categorizar automáticamente los gastos, verificar las políticas de gastos y facilitar la presentación de informes",
   },
   {
     title: "Trámites de reclamo",
-    description: "",
+    description:
+      "El proceso de trámites de reclamo en el sector de seguros implica la presentación, evaluación y resolución de reclamaciones por parte de los asegurados. Tradicionalmente, este proceso ha sido manual y requiere una gran cantidad de tiempo y recursos. Sin embargo, mediante la automatización robótica de procesos (RPA), es posible agilizar y mejorar significativamente este proceso.Con la implementación de RPA, los bots pueden encargarse de recopilar la información necesaria de los asegurados, verificar la validez de los reclamos, procesar la documentación requerida y realizar las correspondientes notificaciones a las partes involucradas. Esto reduce la carga de trabajo manual, acelera los tiempos de respuesta y mejora la precisión en la evaluación de los reclamos.Además, la automatización de los trámites de reclamo permite una mayor transparencia y seguimiento del proceso. Los asegurados pueden recibir actualizaciones en tiempo real sobre el estado de sus reclamos y acceder a la información relevante de manera más rápida y eficiente.",
   },
   {
     title: "Tramitación de apelaciones",
-    description: "",
+    description:
+      "La tramitación de apelaciones en el ámbito de los seguros implica revisar y resolver las apelaciones presentadas por los asegurados que no están satisfechos con las decisiones iniciales tomadas en relación con sus reclamos. Este proceso puede ser complejo y requiere un análisis exhaustivo de la documentación y la legislación aplicable.Mediante la automatización robótica de procesos (RPA), es posible agilizar y mejorar la tramitación de apelaciones en el sector de seguros. Los bots pueden encargarse de recopilar y analizar la información relevante, aplicar las normativas y políticas correspondientes, y generar informes detallados que respalden las decisiones tomadas.La implementación de RPA en la tramitación de apelaciones proporciona una mayor eficiencia en el proceso, reduciendo los tiempos de respuesta y mejorando la consistencia en la toma de decisiones. Además, permite un seguimiento más preciso y transparente de las apelaciones, brindando a los asegurados una mayor confianza en el proceso y una experiencia más satisfactoria.",
   },
   {
     title:
       "Recopilación y liquidación de primas (emisión de estados de cuenta)",
-    description: "",
-  },
-  {
-    title: "Recursos Humanos",
-    description: "",
+    description:
+      "La recopilación y liquidación de prima es un proceso fundamental en el sector de seguros, que implica la recaudación de las primas de los asegurados y la correcta asignación de los pagos correspondientes. Este proceso puede ser propenso a errores y requiere una gestión precisa y eficiente.Mediante la automatización robótica de procesos (RPA), es posible optimizar y agilizar la recopilación y liquidación de prima en el sector de seguros. Los bots pueden encargarse de recopilar los datos de los asegurados, calcular las primas de manera automática, generar las facturas correspondientes y gestionar los pagos y la asignación de los mismos.",
   },
   {
     title: "Preselección de candidatos",
@@ -75,7 +72,8 @@ const asistentesDigitales = () => [
   },
   {
     title: "Verificación de antecedentes",
-    description: "",
+    description:
+      "es un proceso que implica varios pasos rutinarios, como programar entrevistas y mantener registros actualizados. Robocorp presenta un caso de estudio en el que implementaron un programa de automatización en 8 semanas, reduciendo el trabajo manual en un 40%.La automatización proporcionada por Robocorp permite agilizar y simplificar este proceso de verificación. Mediante la implementación de un programa de automatización en tan solo 8 semanas, Robocorp logró reducir el trabajo manual asociado a la verificación del historial laboral en un 40%.La implementación de la automatización optimiza la gestión de entrevistas, al programarlas de manera eficiente y mantener un seguimiento adecuado de los registros. Esto resulta en un proceso más ágil y preciso, evitando posibles errores humanos y asegurando una verificación confiable del historial laboral de los candidatos.",
   },
   {
     title: "Pago de planilla",
@@ -83,7 +81,8 @@ const asistentesDigitales = () => [
   },
   {
     title: "Gestión de ausencias",
-    description: "",
+    description:
+      "El registro preciso de las ausencias y vacaciones por parte del personal suele ser deficiente. Los empleados pueden estar confundidos o no estar familiarizados con el sistema de gestión de ausencias. Además, pueden evitar aprender o recordar cómo utilizar el sistema, ya que una vez que lo hagan, se les exigirá completar todas sus ausencias, lo cual puede resultar desalentador.La solución más sencilla es permitir que las personas decidan sus vacaciones siempre que cumplan con sus responsabilidades laborales. Esto se conoce como política de vacaciones ilimitadas y puede aumentar la autonomía del equipo y ahorrarles una gran cantidad de trámites burocráticos.Un ejemplo real es proporcionado por Robocorp. Implementaron la automatización para la gestión de certificados médicos en SAP en 3 meses para un proveedor de servicios de recursos humanos en Alemania. La implementación de RPA redujo el esfuerzo manual al 10% de los niveles anteriores a la automatización y disminuyó el tiempo de procesamiento en un 85%.",
   },
 ];
 //
@@ -247,12 +246,29 @@ const Rpa = () => {
       {/* sectores  */}
 
       {/* asistentes digitales  */}
-      <div className="py-28">
-        <h1 className="font-fjalla text-5xl font-semibold">
+      <div className=" py-28">
+        <h1 className="mb-12 text-center font-fjalla text-5xl font-semibold">
           Asistentes digitales
         </h1>
+        <div className="mx-auto grid w-[95%] max-w-[900px] grid-cols-2 gap-5  px-2 md:grid-cols-4 2xl:max-w-[1200px]">
+          {asistentesDigitales.map((item, index) => (
+            <span
+              onClick={() =>
+                setModal({ title: item.title, description: item.description })
+              }
+              key={index}
+              className="gradient-border flex cursor-pointer items-center justify-center  rounded-2xl  p-3 text-center text-lg  font-semibold tracking-wide 2xl:text-xl"
+            >
+              {item.title}
+            </span>
+          ))}
+        </div>
+        <Modal
+          title={modal.title}
+          description={modal.description}
+          setModal={setModal}
+        />
       </div>
-
       {/* asistentes digitales  */}
     </div>
   );
