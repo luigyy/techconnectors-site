6c3e738e2defffe57440177a4c5352dbf9150c30
luigyy <luigy.valverde@ucr.ac.cr>
2023-05-19 15:55:15 -0600

    header for about us page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,32 +2,20 @@ import React from "react";
 
 const _index = () => {
   return (
-    <div>
-      <div className="dotted-bg h-screen px-32 py-12">
-        <h1 className="pb-16 text-center font-fjalla text-5xl font-semibold">
+    <div className="overflow-hidden">
+      <div className="dotted-bg relative min-h-[65vh] overflow-hidden  px-12 py-12">
+        <h1 className="pb-7 text-center font-fjalla text-5xl font-semibold">
           Quienes somos?
         </h1>
-
-        <div className="flex flex-wrap justify-around gap-4 md:gap-y-7">
-          <p className=" glow-on-hover max-w-[400px] rounded-2xl bg-white px-6 py-2   ">
-            Somos una empresa enfocada en acelerar la transformación digital
-            Preparamos a las empresas para el uso de tecnologías que humanizan
-            las operaciones empresariales .
-          </p>
-          <br />
-          <p className=" glow-on-hover max-w-[400px] rounded-2xl bg-white  px-6 py-2 ">
-            Todo comienza por la estrategia, es por eso que nuestras soluciones
-            permiten aplicar implementaciones ágiles con tecnología de punta.
-          </p>
-          <br />
-          <p className="glow-on-hover max-w-[400px] rounded-2xl border bg-white px-6  py-2 shadow-md ">
-            Nos enfocamos en crear estrategias integrales para las empresas.
-            Basados en un modelo ágil de mejora continua de 6 etapas logramos
-            acelerar la implementación de tecnología y aumentar la experiencia
-            del usuario a través de talleres de humanización y el descubrimiento
-            de las necesidades reales organizacionales.
-          </p>
-        </div>
+        <p className="mx-auto w-[95%] max-w-[350px] text-center text-2xl md:max-w-[800px] md:text-4xl 2xl:max-w-[1200px]">
+          Somos una empresa enfocada en acelerar la transformación digital
+          Preparamos a las empresas para el uso de tecnologías que humanizan las
+          operaciones empresariales
+        </p>
+        {/* blobs  */}
+        <div className="about-blob-1 absolute left-0 top-0 h-80  w-80 rounded-full bg-[#bf1922] opacity-10 mix-blend-multiply blur-3xl "></div>
+        <div className="about-blob-2 absolute bottom-0 right-0 h-80 w-80 rounded-full bg-[#bf1922] opacity-10 mix-blend-multiply blur-3xl "></div>
+        {/* blobs  */}
       </div>
       <div className="mt-12 py-20 md:mt-0">
         <h1 className="text-center font-fjalla text-5xl tracking-wide">
